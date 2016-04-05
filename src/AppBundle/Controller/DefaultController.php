ffb157832d15843846a549b9de3d74f3ecd61f72
Léopold Pelissier <leopold.pelissier@gmail.com>
2016-04-05 11:43:18 -0400

    fix problem with https images

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,11 +17,41 @@ class DefaultController extends Controller
     public function htmlToPdfAction(Request $request)
     {
         if ($request->getMethod() == 'POST') {
-            $htmlContent = $request->request->get('html_content');
-            $pdfContent = $this->get('knp_snappy.pdf')->getOutputFromHtml(utf8_decode($htmlContent));
+            $baseurl = $request->getScheme() . '://' . $request->getHttpHost();
+
+            $htmlContent = utf8_decode($request->request->get('html_content'));
+            $htmlContent = $this->replaceHttps($htmlContent, $baseurl);
+            $pdfContent = $this->get('knp_snappy.pdf')->getOutputFromHtml($htmlContent);
             return new JsonResponse(array('pdf_content' => utf8_encode($pdfContent)));
         }else{
             return $this->render('default/htmltopdf.html.twig');
         }
+    }
+
+    protected function replaceHttps($html, $baseurl)
+    {
+        if(preg_match_all('!https://[a-z0-9\-\.\/]+\.(?:jpe?g|png|gif|svg)!Ui', $html, $matches)){
+            $matches = array_pop($matches);
+            foreach($matches as $key => $url){
+                $explode = explode('.',$url);
+                $extension = end($explode);
+
+                $source = $url;
+                $target = $this->get('kernel')->getRootDir() . '/../web/tmp/file'.$key.'.'.$extension;
+                $ch = curl_init($source);
+                $fp = fopen($target, "wb");
+
+                curl_setopt($ch, CURLOPT_FILE, $fp);
+                curl_setopt($ch, CURLOPT_HEADER, 0);
+
+                curl_exec($ch);
+                curl_close($ch);
+                fclose($fp);
+
+                $html = str_replace($url, $baseurl.'/tmp/file'.$key.'.'.$extension, $html);
+            }
+        }
+
+        return $html;
     }
 }
