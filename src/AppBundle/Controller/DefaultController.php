16f9bba37f832dd121d6d083e87377810442e8c2
Léopold Pelissier <leopold.pelissier@gmail.com>
2016-04-05 15:56:57 -0400

    add testing section on the home

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,9 +7,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function indexAction(Request $request)
+    {
+        $data = array();
+        $form = $this->createFormBuilder($data)
+            ->add('html_content', TextareaType::class, array(
+                'attr' => array('cols' => '50', 'rows' => '15'),
+                'label' => false
+            ))
+            ->add('generer', SubmitType::class, array(
+                'label' => 'Générer le pdf !'
+            ))
+            ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            $data = $form->getData();
+
+            $baseurl = $request->getScheme() . '://' . $request->getHttpHost();
+            $pdfContent = $this->getContentPdf($data['html_content'], $baseurl);
+
+            return new Response(
+                $pdfContent,
+                200,
+                array(
+                    'Content-Type'          => 'application/pdf',
+                    'Content-Disposition'   => 'attachment; filename="file.pdf"'
+                )
+            );
+        }
+
+        return $this->render('default/index.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
+    }
 
     /**
      * @Route("/htmltopdf", name="htmltopdf")
@@ -19,18 +60,25 @@ class DefaultController extends Controller
         if ($request->getMethod() == 'POST') {
             $baseurl = $request->getScheme() . '://' . $request->getHttpHost();
 
-            $htmlContent = utf8_decode($request->request->get('html_content'));
-            $htmlContent = $this->replaceHttps($htmlContent, $baseurl);
-            $pdfContent = $this->get('knp_snappy.pdf')->getOutputFromHtml($htmlContent);
-            return new JsonResponse(array('pdf_content' => utf8_encode($pdfContent)));
-        }else{
-            return $this->render('default/htmltopdf.html.twig');
+            $htmlContent = $request->request->get('html_content');
+
+            return new JsonResponse(array('pdf_content' => utf8_encode($this->getContentPdf($htmlContent, $baseurl))));
         }
+    }
+
+    public function getContentPdf($htmlContent, $baseurl)
+    {
+        $htmlContent = utf8_decode($htmlContent);
+        $htmlContent = $this->replaceHttps($htmlContent, $baseurl);
+        $pdfContent = $this->get('knp_snappy.pdf')->getOutputFromHtml($htmlContent, array(
+            'load-error-handling' => 'ignore',
+        ));
+        return $pdfContent;
     }
 
     protected function replaceHttps($html, $baseurl)
     {
-        if(preg_match_all('!https://[a-z0-9\-\.\/]+\.(?:jpe?g|png|gif|svg)!Ui', $html, $matches)){
+        if(preg_match_all('!https://[a-z0-9\-\.\/\?\=\&\#]+\.(?:jpe?g|png|gif|svg|eot|woff|ttf)!Ui', $html, $matches)){
             $matches = array_pop($matches);
             foreach($matches as $key => $url){
                 $explode = explode('.',$url);
